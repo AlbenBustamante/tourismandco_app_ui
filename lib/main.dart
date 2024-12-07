b0730c8d939b22a9b70418c930789b7ff9332952
Alben Bustamante <albenbustamante@gmail.com>
2024-12-07 12:22:18 -0500

    update: routes and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tourismandco/screens/location_detail/location_detail.dart';
 import 'package:tourismandco/screens/locations/locations.dart';
 import 'package:tourismandco/style.dart';
 
@@ -11,7 +12,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const Locations(), theme: _theme());
+    return MaterialApp(
+        home: const Locations(), theme: _theme(), onGenerateRoute: _routes);
   }
 
   ThemeData _theme() {
@@ -19,5 +21,20 @@ class MainApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(titleTextStyle: appBarTextStyle),
         textTheme: const TextTheme(
             titleLarge: titleLargeTextStyle, bodyMedium: bodyTextMediumStyle));
+  }
+
+  Route _routes(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return _route(const Locations());
+      case '/location-detail':
+        return _route(LocationDetail(settings.arguments as int));
+      default:
+        return _route(const Locations());
+    }
+  }
+
+  Route<dynamic> _route(Widget view) {
+    return MaterialPageRoute(builder: (_) => view);
   }
 }
--- lib/screens/locations/locations.dart
+++ lib/screens/locations/locations.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tourismandco/models/location.dart';
-import 'package:tourismandco/screens/location_detail/location_detail.dart';
 
 class Locations extends StatelessWidget {
   const Locations({super.key});
@@ -14,15 +13,14 @@ class Locations extends StatelessWidget {
         body: ListView(
             children: locations
                 .map((location) => GestureDetector(
-                    onTap: _onLocationTap(context, location.id),
+                    onTap: () {
+                      _onLocationTap(context, location.id);
+                    },
                     child: Text(location.name)))
                 .toList()));
   }
 
   _onLocationTap(BuildContext context, int locationId) {
-    if (context.mounted) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => LocationDetail(locationId)));
-    }
+    Navigator.pushNamed(context, '/location-detail', arguments: locationId);
   }
 }
